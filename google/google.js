baa8f070b6d352b091aac080aa21b92eea97df6f
cotne nazarashvili <cotne.nazarashvili@gmail.com>
2015-04-09 13:21:02 +0400

    in google.js change from snake_case to camelCase and fix bug of more button

--- google/google.js
+++ google/google.js
@@ -1,40 +1,35 @@
-function translate_link(val){
-	return 'https://translate.google.com/#auto/auto/'
-	+ encodeURIComponent( val );
-};
-function update_href(translate_button_a,e){
-	translate_button_a.setAttribute('href', translate_link( this.value ) );
-};
-function insert_translate_button(){
+function translateLink(text) {
+	return 'https://translate.google.com/#auto/auto/' + encodeURIComponent(text);
+}
+
+function updateHref(translateButtonA, e) {
+	translateButtonA.href = translateLink(this.value);
+}
+
+function insertTranslateButton() {
 	var input = document.getElementById('lst-ib');
-	var more_button = document.getElementById('hdtb_more');
-	var more_div = document.getElementById('hdtb_more_mn');
-	if ( input && more_button && more_div ) {
-		var translate_button = document.createElement('div');
-		translate_button.setAttribute('class','hdtb_mitem');
-		var translate_button_a = document.createElement('a');
-		translate_button_a.setAttribute('class','q qs');
-		translate_button_a.setAttribute('href', translate_link( input.value ) );
-		translate_button_a.innerText = 'Translate';
-		translate_button.appendChild(translate_button_a);
-		if (!input.onkeyup) {
-			input.onkeyup = update_href.bind(input,translate_button_a)
-		};
-		more_button.parentNode.insertBefore( translate_button, more_button );
-		while(more_div.childNodes[0]){
-			more_button.parentNode.insertBefore( more_div.childNodes[0], more_button );
-		};
-		more_button.style.display = 'none';
-		more_div.style.display = 'none';
-	};
-};
-document.addEventListener('DOMContentLoaded', function(){
-	insert_translate_button();
-});
-document.addEventListener('DOMNodeInserted', function(e){
+	var moreButton = document.getElementById('hdtb_more');
+	var moreDiv = document.getElementById('hdtb_more_mn');
+	if (input && moreButton && moreDiv) {
+		var translateButton = document.createElement('div');
+		translateButton.className = 'hdtb_mitem';
+		var translateButtonA = document.createElement('a');
+		translateButtonA.className = 'q qs';
+		translateButtonA.href = translateLink(input.value);
+		translateButtonA.innerText = 'Translate';
+		translateButton.appendChild(translateButtonA);
+		if ( ! input.onkeyup) {
+			input.onkeyup = updateHref.bind(input, translateButtonA)
+		}
+		moreButton.parentNode.insertBefore(translateButton, moreButton);
+	}
+}
+
+function DOMNodeInserted(e) {
 	if (e.target.id == 'hdtb' || (e.target.querySelector && e.target.querySelector('#hdtb'))) {
-		insert_translate_button();
-	};
-});
+		insertTranslateButton();
+	}
+}
 
-
+document.addEventListener('DOMContentLoaded', insertTranslateButton);
+document.addEventListener('DOMNodeInserted', DOMNodeInserted);
