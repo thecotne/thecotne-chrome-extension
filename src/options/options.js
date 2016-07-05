e3f1a46b87a6dabecb9e4d1267ee3432d693e0d9
cotne nazarashvili <cotne.95@gmail.com>
2016-07-05 19:29:41 +0400

    add options page

---
+++ src/options/options.js
@@ -0,0 +1,15 @@
+const optionElems = document.querySelectorAll('[data-options]')
+
+for (const elem of optionElems) {
+    if (elem.type === 'checkbox') {
+        elem.addEventListener('change', e => {
+            chrome.storage.sync.set({
+                [e.target.name]: e.target.checked,
+            })
+        })
+
+        chrome.storage.sync.get(elem.name, items => {
+            elem.checked = items[elem.name] || false
+        })
+    }
+}
