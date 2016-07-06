aa1dde56f09cf2d319ccf63a0e903dcdbb611e31
cotne nazarashvili <cotne.95@gmail.com>
2016-07-07 02:01:03 +0400

    add scroll back fixes #1

--- scroll/scroll.js
+++ scroll/scroll.js
@@ -1,34 +1,33 @@
-var scrollTopBtn = null;
+chrome.storage.sync.get('scrollTop', ({scrollTop}) => {
+    if (scrollTop) {
+        const scrollTopBtn = document.createElement('a')
 
-function scrollTopBtnClick() {
-	scrollLoop((new Date()).getTime(), 200, document.body.scrollTop);
-}
+        scrollTopBtn.classList.add('thecotne-scroll-top-btn')
+        scrollTopBtn.addEventListener('click', e => {
+            scrollLoop((new Date()).getTime(), 200, document.body.scrollTop)
+        })
 
-function scroll() {
-	if (window.pageYOffset > 300) {
-		if (!scrollTopBtn) {
-			scrollTopBtn = document.createElement('a');
-			scrollTopBtn.classList.add('thecotne_scroll_top_btn');
-			scrollTopBtn.addEventListener('click', scrollTopBtnClick, false);
-			document.body.appendChild(scrollTopBtn);
-		}
-		scrollTopBtn.style.opacity = '1';
-		scrollTopBtn.style.pointerEvents = 'all';
-	} else {
-		if (scrollTopBtn) {
-			scrollTopBtn.style.opacity = '0';
-			scrollTopBtn.style.pointerEvents = 'none';
-		}
-	}
-}
+        document.body.appendChild(scrollTopBtn)
 
-document.addEventListener('scroll', scroll, false);
+        document.addEventListener('scroll', e => {
+            if (window.pageYOffset > 300) {
+                scrollTopBtn.style.opacity = '1'
+                scrollTopBtn.style.pointerEvents = 'all'
+            } else {
+                scrollTopBtn.style.opacity = '0'
+                scrollTopBtn.style.pointerEvents = 'none'
+            }
+        })
 
-function scrollLoop(beginTime, duration, scrollFrom) {
-	var timePassed = (new Date()).getTime() - beginTime;
-	var timeLeft = duration - timePassed;
-	document.body.scrollTop = timeLeft * (scrollFrom / duration)
-	if (timePassed < duration) {
-		requestAnimationFrame(scrollLoop.bind(undefined, beginTime, duration, scrollFrom));
-	}
-}
+        function scrollLoop(beginTime, duration, scrollFrom) {
+            const timePassed = (new Date()).getTime() - beginTime
+            const timeLeft = duration - timePassed
+
+            document.body.scrollTop = timeLeft * (scrollFrom / duration)
+
+            if (timePassed < duration) {
+                requestAnimationFrame(() => scrollLoop(beginTime, duration, scrollFrom))
+            }
+        }
+    }
+})
