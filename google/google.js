708bb0d7f044f77fcac17b7bf98cca599cd09dd4
cotne nazarashvili <cotne.95@gmail.com>
2016-07-08 00:11:35 +0400

    update google script

--- google/google.js
+++ google/google.js
@@ -1,33 +1,33 @@
 function translateLink(text) {
-	return 'https://translate.google.com/#auto/auto/' + encodeURIComponent(text);
-}
-
-function updateHref(translateButtonA, e) {
-	translateButtonA.href = translateLink(this.value);
+    return 'https://translate.google.com/#auto/auto/' + encodeURIComponent(text)
 }
 
 function insertTranslateButton() {
-	var input = document.getElementById('lst-ib');
-	var moreButton = document.getElementById('hdtb-more');
-	if (input && moreButton) {
-		var translateButton = document.createElement('div');
-		translateButton.className = 'hdtb-mitem hdtb-imb';
-		var translateButtonA = document.createElement('a');
-		translateButtonA.className = 'q qs';
-		translateButtonA.href = translateLink(input.value);
-		translateButtonA.innerText = 'Translate';
-		translateButton.appendChild(translateButtonA);
-		if ( ! input.onkeyup) {
-			input.onkeyup = updateHref.bind(input, translateButtonA)
-		}
-		moreButton.parentNode.insertBefore(translateButton, moreButton);
-	}
+    const input = document.getElementById('lst-ib')
+    const moreButton = document.getElementById('hdtb-more')
+    if (input && moreButton) {
+        const translateButton = document.createElement('div')
+        translateButton.className = 'hdtb-mitem hdtb-imb'
+
+        const translateButtonA = document.createElement('a')
+        translateButtonA.className = 'q qs'
+        translateButtonA.href = translateLink(input.value)
+        translateButtonA.innerText = 'Translate'
+
+        translateButton.appendChild(translateButtonA)
+
+        input.addEventListener('keyup', e => {
+            translateButtonA.href = translateLink(e.target.value)
+        })
+
+        moreButton.parentNode.insertBefore(translateButton, moreButton)
+    }
 }
 
 function DOMNodeInserted(e) {
-	if (e.target.id == 'hdtb' || (e.target.querySelector && e.target.querySelector('#hdtb'))) {
-		insertTranslateButton();
-	}
+    if (e.target.id == 'hdtb' || (e.target.querySelector && e.target.querySelector('#hdtb'))) {
+        insertTranslateButton()
+    }
 }
 
-document.addEventListener('DOMNodeInserted', DOMNodeInserted);
+document.addEventListener('DOMNodeInserted', DOMNodeInserted)
