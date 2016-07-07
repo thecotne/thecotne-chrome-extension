3bb4be133ac5d4304ac7b61311af6d27526a6c47
cotne nazarashvili <cotne.95@gmail.com>
2016-07-07 19:01:03 +0400

    update forumge script

--- forumge/forumge.js
+++ forumge/forumge.js
@@ -1,101 +1,65 @@
-function insertText(word, field) {
-	if (document.selection) {
-		field.focus();
-		var sel = document.selection.createRange();
-		sel.text = word;
-		sel.select();
-	} else if (field.selectionStart || field.selectionStart == "0") {
-		field.focus();
-		var start = field.selectionStart;
-		var end = field.selectionEnd;
-		var scroll = field.scrollTop;
-		field.value = field.value.substring(0, start) + word + field.value.substring(end, field.value.length);
-		field.focus();
-		field.selectionStart = start + word.length;
-		field.selectionEnd = start + word.length;
-		field.scrollTop = scroll;
-	} else {
-		field.focus();
-		field.value += word;
-		field.focus();
-	}
+const ClientID = 'c85f84102eecd3e'
+
+const postInput = document.querySelector('[name=Post]')
+
+const uploadButton = document.createElement('a')
+uploadButton.href = 'javascript:'
+uploadButton.classList.add('thecotne-imageupload')
+
+const uploadInput = document.createElement('input')
+uploadInput.type = 'file'
+uploadInput.style.display = 'none'
+
+postInput.parentNode.insertBefore(uploadButton, postInput)
+postInput.parentNode.insertBefore(uploadInput, postInput)
+
+uploadInput.addEventListener('change', e => {
+    uploadButton.classList.add('loading')
+
+    const formData = new FormData()
+    formData.append('image', uploadInput.files[0])
+
+    fetch('https://api.imgur.com/3/image', {
+        method: 'POST',
+        body: formData,
+        headers: new Headers({
+            'Accept': 'application/json',
+            'Authorization': `Client-ID ${ClientID}`,
+        }),
+    })
+    .then(response => response.json())
+    .then(response => {
+        uploadButton.classList.remove('loading')
+        insertText(`[IMG]${response.data.link}[/IMG]`, postInput)
+    })
+    .catch(err => {
+        alert("ფოტოს ატვირთვა ვერ მოხერხდა")
+    })
+})
+
+uploadButton.addEventListener('click', e => {
+    const clickEvent = document.createEvent("HTMLEvents")
+    clickEvent.initEvent('click', true, true)
+    uploadInput.dispatchEvent(clickEvent)
+})
+
+function insertText(text, field) {
+    if (field.selectionStart > -1) {
+        field.focus()
+
+        const beforeSelection = field.value.substring(0, field.selectionStart)
+        const afterSelection = field.value.substring(field.selectionEnd, field.value.length)
+
+        field.value = beforeSelection + text + afterSelection
+
+        field.focus()
+
+        field.selectionStart += text.length
+        field.selectionEnd += text.length
+
+    } else {
+        field.focus()
+        field.value += text
+        field.focus()
+    }
 }
-
-function include (url, ext) {
-	if (ext == undefined || ext == true) {
-		url = chrome.extension.getURL(url);
-	};
-	if (url.substr(-3, 3) == ".js") {
-		var element = document.createElement('script');
-		element.type = "text/javascript";
-		element.src = url;
-	} else if (url.substr(-4, 4) == ".css") {
-		var element = document.createElement('link');
-		element.rel = "stylesheet";
-		element.type = "text/css";
-		element.href = url;
-	};
-	document.head.appendChild(element);
-}
-
-include('forumge/forumge.css');
-
-var _DOMParser = new DOMParser();
-
-var postInput = document.getElementsByName('Post')[0];
-
-var addImageIcon = document.createElement('a');
-addImageIcon.href = 'javascript:';
-addImageIcon.id = 'forum_plus_imagepost';
-
-var addImageInput = document.createElement('input');
-addImageInput.type = 'file';
-addImageInput.style.display = 'none';
-
-postInput.parentNode.insertBefore(addImageIcon, postInput);
-postInput.parentNode.insertBefore(addImageInput, postInput);
-
-addImageInput.onchange = function () {
-	addImageIcon.classList.add('loading');
-
-	var formData = new FormData();
-	var file = this.files[0];
-	reader = new FileReader();
-	reader.readAsDataURL(file);
-	formData.append('file_1', file);
-	formData.append("reducesize", "off");
-	formData.append("maxsize", "640");
-	formData.append("ispublic", "on");
-	formData.append("submit", "\u10d0\u10e2)u10d5\u10d8\u10e0\u10d7\u10d5\u10d0!");
-	formData.append("image_text", "");
-	formData.append("text_size", "3");
-	formData.append("text_color", "000000");
-	formData.append("text_transparency", "6");
-	formData.append("text_halign", "-1");
-	formData.append("text_valign", "-1");
-	formData.append("text_font", "1");
-
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', 'http://www.picz.ge/scripts/up.py');
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState == 4) {
-			addImageIcon.classList.remove('loading');
-
-			var responseDOM = _DOMParser.parseFromString(xhr.responseText, 'text/html')
-			var imageURL = responseDOM.getElementsByTagName('input')[0].value;
-			if (imageURL.substring(0, 4) == "http") {
-				insertText("[IMG]" + imageURL + "[/IMG]", postInput);
-			} else {
-				alert("ფოტოს ატვირთვა ვერ მოხერხდა");
-			}
-		}
-	}
-	xhr.send(formData);
-};
-
-addImageIcon.onclick = function () {
-	var _event = document.createEvent("HTMLEvents");
-	_event.initEvent('click', true, true);
-	_event.eventName = 'click';
-	addImageInput.dispatchEvent(_event);
-};
