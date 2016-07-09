46c009417756d00fef06e95c3c2ed53722f14aba
cotne nazarashvili <cotne.95@gmail.com>
2016-07-10 03:20:13 +0400

    update google script

--- google/google.js
+++ google/google.js
@@ -2,21 +2,31 @@ function translateLink(text) {
   return `https://translate.google.com/#auto/auto/${encodeURIComponent(text)}`
 }
 
+function createElement(tagName, props, ...childs) {
+  const element = document.createElement(tagName)
+  for (const propName in props) {
+    element[propName] = props[propName]
+  }
+  for (const child of childs) {
+    element.appendChild(child)
+  }
+  return element
+}
+
 function insertTranslateButton() {
   const input = document.getElementById('lst-ib')
   const moreButton = document.getElementById('hdtb-more')
   const translateButtonCheck = ! document.querySelector('.thecotne-translate-btn')
 
   if (input && moreButton && translateButtonCheck) {
-    const translateButton = document.createElement('div')
-    translateButton.className = 'hdtb-mitem hdtb-imb'
-
-    const translateButtonA = document.createElement('a')
-    translateButtonA.className = 'q qs thecotne-translate-btn'
-    translateButtonA.href = translateLink(input.value)
-    translateButtonA.innerText = 'Translate'
-
-    translateButton.appendChild(translateButtonA)
+    const translateButtonA = createElement('a', {
+      className: 'q qs thecotne-translate-btn',
+      href: translateLink(input.value),
+      innerText: 'Translate',
+    })
+    const translateButton = createElement('div', {
+      className: 'hdtb-mitem hdtb-imb',
+    }, translateButtonA)
 
     input.addEventListener('keyup', e => {
       translateButtonA.href = translateLink(e.target.value)
